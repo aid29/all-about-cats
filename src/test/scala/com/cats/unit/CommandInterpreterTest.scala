ff8d51511a6612ffc7336b02dc7222de7cd026ae
dengai <dengai129@hotmail.com>
2016-02-29 11:34:28 +0000

    Ai styling the tests and formatting the codes

--- src/test/scala/com/cats/unit/CommandInterpreterTest.scala
+++ src/test/scala/com/cats/unit/CommandInterpreterTest.scala
@@ -10,44 +10,51 @@ import scala.concurrent.{Future, ExecutionContext}
 import ExecutionContext.Implicits.global
 
 class CommandInterpreterTest extends FunSpec {
-  describe("command interpreter") {
-    it("call api to get a random cat image and call repository to save the image to the file when receive 'file' command") {
-      val catApi = mock[CatsApi]
-      val catRepo = mock[CatImageRepository]
 
-      val apiRes:Xor[CatsError, CatImage] = Xor.right(CatImage("url1","cat", None))
-      val repoRes:Xor[CatsError, CatImage] = Xor.right(CatImage("url1","cat", Some("xxx")))
+  it("call api to get a random cat image and call repository to save the image to the file when receive 'file' command") {
+    val catApi = mock[CatsApi]
+    val catRepo = mock[CatImageRepository]
 
-      when(catApi.catImage).thenReturn(Future{apiRes})
-      when(catRepo.saveImage(Xor.right(CatImage("url1","cat", None)))).thenReturn(Future{repoRes})
+    val apiRes: Xor[CatsError, CatImage] = Xor.right(CatImage("url1", "cat", None))
+    val repoRes: Xor[CatsError, CatImage] = Xor.right(CatImage("url1", "cat", Some("xxx")))
 
-      val commandInterpreter = new CommandInterpreter(catApi, catRepo)
-      val executor = commandInterpreter.runCommand(Array("file"))
-      executor.runAndWait should be(repoRes)
-    }
+    when(catApi.catImage).thenReturn(Future {
+      apiRes
+    })
+    when(catRepo.saveImage(Xor.right(CatImage("url1", "cat", None)))).thenReturn(Future {
+      repoRes
+    })
 
-    it("call api to get a list of cat's categories when receive 'categories' command") {
-      val catApi = mock[CatsApi]
-      val catRepo = mock[CatImageRepository]
-      val res:Xor[CatsError, List[CatCategory]] = Xor.right(List(CatCategory(1,"test")))
+    val commandInterpreter = new CommandInterpreter(catApi, catRepo)
+    val executor = commandInterpreter.runCommand(Array("file"))
+    executor.runAndWait should be(repoRes)
+  }
 
-      when(catApi.catCategories).thenReturn(Future{res})
+  it("call api to get a list of cat's categories when receive 'categories' command") {
+    val catApi = mock[CatsApi]
+    val catRepo = mock[CatImageRepository]
+    val res: Xor[CatsError, List[CatCategory]] = Xor.right(List(CatCategory(1, "test")))
 
-      val commandInterpreter = new CommandInterpreter(catApi, catRepo)
-      val executor = commandInterpreter.runCommand(Array("categories"))
-      executor.runAndWait should be(res)
-    }
+    when(catApi.catCategories).thenReturn(Future {
+      res
+    })
 
-    it("call api to get a cat's fact when receive 'fact' command") {
-      val catApi = mock[CatsApi]
-      val catRepo = mock[CatImageRepository]
-      val res:Xor[CatsError, String] = Xor.right("cat fact")
+    val commandInterpreter = new CommandInterpreter(catApi, catRepo)
+    val executor = commandInterpreter.runCommand(Array("categories"))
+    executor.runAndWait should be(res)
+  }
 
-      when(catApi.catFact).thenReturn(Future{res})
+  it("call api to get a cat's fact when receive 'fact' command") {
+    val catApi = mock[CatsApi]
+    val catRepo = mock[CatImageRepository]
+    val res: Xor[CatsError, String] = Xor.right("cat fact")
 
-      val commandInterpreter = new CommandInterpreter(catApi, catRepo)
-      val executor = commandInterpreter.runCommand(Array("fact"))
-      executor.runAndWait should be(res)
-    }
+    when(catApi.catFact).thenReturn(Future {
+      res
+    })
+
+    val commandInterpreter = new CommandInterpreter(catApi, catRepo)
+    val executor = commandInterpreter.runCommand(Array("fact"))
+    executor.runAndWait should be(res)
   }
 }
